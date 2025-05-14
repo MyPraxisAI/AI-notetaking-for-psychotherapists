800f8bcf336c8527b7fb5de9c8e6939825c93cc1
gtmax <max@mypraxis.ai>
2025-05-14 12:01:17 +0300

    last build fixes

--- packages/shared-common/index.js
+++ packages/shared-common/index.js
@@ -1,3 +1,4 @@
 // This file is used to provide a clean entry point for the package
 // It's particularly important for Docker builds
-export * from './dist/index.js';
+// Using CommonJS syntax for compatibility with Node.js
+module.exports = require('./dist/index.js');
