9c02ff940837e7a215acf14d146cb6ba4d520e7f
Max Rozenoer <max@mypraxis.ai>
2025-07-06 15:47:24 +0300

    YSTM-753 posthog analytics in addition to ga (#73)

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -1,10 +1,16 @@
 import { createGoogleAnalyticsService } from '@kit/google-analytics';
+import { createPostHogAnalyticsService } from '@kit/posthog';
 
 import { createAnalyticsManager } from './analytics-manager';
 import type { AnalyticsManager } from './types';
+import { NullAnalyticsService } from './null-analytics-service';
+
+// analytics only enabled in production
+const isProd = process.env.NODE_ENV === 'production';
 
 export const analytics: AnalyticsManager = createAnalyticsManager({
   providers: {
-    'google-analytics': createGoogleAnalyticsService,
+    'google-analytics': isProd ? createGoogleAnalyticsService : () => NullAnalyticsService,
+    posthog: isProd ? createPostHogAnalyticsService : () => NullAnalyticsService,
   },
 });
