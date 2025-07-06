eda2a77f91e59460eed6a1069601f68d99159db2
gtmax <max@mypraxis.ai>
2025-07-06 15:40:00 +0300

    Fix lint

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -3,13 +3,14 @@ import { createPostHogAnalyticsService } from '@kit/posthog';
 
 import { createAnalyticsManager } from './analytics-manager';
 import type { AnalyticsManager } from './types';
+import { NullAnalyticsService } from './null-analytics-service';
 
 // analytics only enabled in production
 const isProd = process.env.NODE_ENV === 'production';
 
 export const analytics: AnalyticsManager = createAnalyticsManager({
   providers: {
-    'google-analytics': isProd ? createGoogleAnalyticsService : undefined as any,
-    posthog: isProd ? createPostHogAnalyticsService : undefined as any,
+    'google-analytics': isProd ? createGoogleAnalyticsService : () => NullAnalyticsService,
+    posthog: isProd ? createPostHogAnalyticsService : () => NullAnalyticsService,
   },
 });
