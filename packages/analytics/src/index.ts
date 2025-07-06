32a2f15ca78a67e96dc2e0935d47613feafc3f87
gtmax <max@mypraxis.ai>
2025-07-06 15:28:56 +0300

    Disable analytics outside of prod, remove sensitive

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -4,9 +4,12 @@ import { createPostHogAnalyticsService } from '@kit/posthog';
 import { createAnalyticsManager } from './analytics-manager';
 import type { AnalyticsManager } from './types';
 
+// analytics only enabled in production
+const isProd = process.env.NODE_ENV === 'production';
+
 export const analytics: AnalyticsManager = createAnalyticsManager({
   providers: {
-    'google-analytics': createGoogleAnalyticsService,
-    posthog: createPostHogAnalyticsService
+    'google-analytics': isProd ? createGoogleAnalyticsService : undefined as any,
+    posthog: isProd ? createPostHogAnalyticsService : undefined as any,
   },
 });
