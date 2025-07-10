bb029ec9581a37435a7275031a9424883214893d
Max Rozenoer <max@mypraxis.ai>
2025-07-10 15:30:04 +0300

    show errors in toasts (#76)

--- apps/web/app/home/(user)/mypraxis/_lib/hooks/use-update-password.ts
+++ apps/web/app/home/(user)/mypraxis/_lib/hooks/use-update-password.ts
@@ -74,7 +74,11 @@ export function useUpdatePassword() {
     },
     onError: (error) => {
       console.error('Error updating password:', error);
-      toast.error(t('hooks.userProfile.passwordUpdatedError'));
+      let message = '';
+      if (error && typeof error === 'object' && 'message' in error && error.message) {
+        message = ': ' + error.message;
+      }
+      toast.error(t('hooks.userProfile.passwordUpdatedError', { message }));
     },
   });
 }
